cce133fdcae298fd47e105c55db42422affd48a7
ennafiaasma-art <ennafiaasma@gmail.com>
2026-09-18 11:50:00 +0100

    la creation de validation request pour favorite controller

--- backend/app/Http/Controllers/FavoriteController.php
+++ backend/app/Http/Controllers/FavoriteController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreFavoriteRequest;
+use App\Models\Favorite;
 use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $favorites = $request->user()->favoriteDestinations;
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $favorites
+        ]);
+    }
+
+    public function store(StoreFavoriteRequest $request)
+    {
+        $userId = $request->user()->id;
+        $destinationId = $request->destination_id;
+
+        $favorite = Favorite::where('user_id', $userId)
+                            ->where('destination_id', $destinationId)
+                            ->first();
+
+        if ($favorite) {
+            $favorite->delete();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Destination retirée des favoris avec succès.'
+            ]);
+        } else {
+            Favorite::create([
+                'user_id' => $userId,
+                'destination_id' => $destinationId
+            ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Destination ajoutée aux favoris avec succès.'
+            ], 201);
+        }
+    }
 }
